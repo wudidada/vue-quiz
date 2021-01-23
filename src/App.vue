37f7c0e29526dd8b93ad721e924e2de65deb5e4a
Jusbin <943135242@qq.com>
2021-01-23 22:17:09 +0800

    Start

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <Header />
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox 
+            :currentQuestion="questions[index]"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    QuestionBox,
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0
+    }
+  },
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10', {
+      method: 'get'
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((jsonData) => {
+        this.questions = jsonData.results
+      })
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template #lead>
+        {{ currentQuestion }}
+      </template>
+
+      <hr class="my-4" />
+
+      <p>List of answers</p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object
+  }
+}
+</script>
